2943c3f0d6cb81021c441edd42428f6901bf5a73
Cecilia Garcia <ceciliagarciagon92@gmail.com>
2020-10-02 11:07:28 +0200

    useContext

---
+++ src/components/01-useState/08-useContext/AppRouter.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route, Redirect
+  } from "react-router-dom";
+
+import { AboutScreen } from './AboutScreen';
+import { HomeScreen } from './HomeScreen';
+import { LoginScreen } from './LoginScreen';
+import { NavBar } from './NavBar';
+
+export const AppRouter = () => {
+    return (
+       <Router>
+           <div>
+
+               <NavBar></NavBar>
+
+                <Switch>
+                    <Route path="/" component={ HomeScreen }/>
+
+                    <Route path="/about" component={ AboutScreen}/>
+                    <Route path="/login" component={ LoginScreen  }/>
+
+                    <Redirect to="/" />
+                   
+                </Switch>
+
+           </div>
+       </Router>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { TodoApp } from './components/01-useState/07-useReducer/TodoApp';
+import { MainApp } from './components/01-useState/08-useContext/MainApp';
+//import { TodoApp } from './components/01-useState/07-useReducer/TodoApp';
 
 
 //import { CallbackHook } from './components/01-useState/06-memos/CallBackHook';
@@ -19,7 +20,7 @@ import { TodoApp } from './components/01-useState/07-useReducer/TodoApp';
 
 
 ReactDOM.render(
-    <TodoApp />,
+    <MainApp/>,
   document.getElementById('root')
 );
 
